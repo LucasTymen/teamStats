0f48954ab901c172d7404220b82e5fa4f2a9c38e
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-14 17:05:02 +0100

    teamStat_Done

--- teamStats.js
+++ teamStats.js
@@ -1,14 +1,44 @@
 const team = {
 	_players: [
-    {firstName:'tommy', lastName: 'guerrero', age: 56, sport:'skateboard'},
-    {firstName:'tony', lastName: 'hawk', age: 54, sport:'skateboard'},
-    {firstName: 'Antoine', lastName: 'Dupont', age: 26, sport:'rugby'},
-    {firstName:'Cyril', lastName: 'Baille', age: 29, sport:'rugby'},
+    {firstName:'tommy', lastName:'guerrero', age: 56, sport:'skateboard'},
     {firstName:'Nicolas', lastName:'Tournat', age: 27, sport:'handball'},
-    {firstName:'Nick', lastName:'Suzuki', age: 23, sport:'ice hockey'}
+    {firstName:'Nick', lastName:'Suzuki', age: 23, sport:'ice hockey'},
+    {firstName:'tony', lastName:'hawk', age: 54, sport:'skateboard'},
+    {firstName:'Antoine', lastName:'Dupont', age: 26, sport:'rugby'},
+    {firstName:'Cyril', lastName:'Baille', age: 29, sport:'rugby'},
     ],
-  _games: [{game:'ice hockey',opponent:[], teamPoints:0, opponentPoints:0},{game:'rugby',opponent:[], teamPoints:0, opponentPoints:0},{game:'handball',opponent:[], teamPoints:0, opponentPoints:0},{game:'skateboard',opponent:[], teamPoints:0, opponentPoints:0}],
-_games: ['ice hockey','rugby','handball','skateboard']
+  _games: [
+    {game:'ice hockey',opponent:'canadiens of Monreal', teamPoints:37, opponentPoints:230},
+    {game:'rugby',opponent:'stade Toulousain', teamPoints:64, opponentPoints:86},
+    {game:'handball',opponent:'les nouveaux mutants', teamPoints:52, opponentPoints:12},
+    {game:'skateboard',opponent:'Monster team', teamPoints:14, opponentPoints:9}
+    ],
+
+    get players() {
+      return this._players;
+     },
+    get games() {
+      return this._games
+     },
+    addPlayer (newFirstName, newLastName, newAge){
+      let player = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        age: newAge
+    };
+    this.players.push(player);
+  },
+  addGame(newOpponent, newTeamPoints, newOpponentPoints){
+    let game = {
+      opponent: newOpponent,
+      teamPoints: newTeamPoints,
+      opponentPoints: newOpponentPoints
+    };
+      this.games.push(game);
+  }
 };
 
-console.log(team._players)
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team._players );
+team.addGame('Titans', 100, 98);
+console.log(team.games);
